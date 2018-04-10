7ee255920b3c2a571053a6d54996e0989391b298
mirecta <miroslav.talasek@seznam.cz>
2018-04-10 21:46:12 +0200

    nice main start point

--- opizcarmp3/main.py
+++ opizcarmp3/main.py
@@ -4,31 +4,22 @@ from lib.RPi_I2C_LCD import LCD
 import rotary
 import time
 
-lcd = LCD()
-
-val = 0
-
-
-def call(a):
-    global val
-    val += a/2.0
-    lcd.set_cursor(row=0)
-    lcd.message("%d    "%int(val))
-    #print val
-
-
-
 
 def main():
-    
+    lcd = LCD()
     lcd.set_backlight(True)
-    rotary.RotaryEncoder(6,10,11,call)
-
-    # first banner
-    lcd.set_cursor(row=0)
-    lcd.message("%d    "%val)
+    
+    rotary.RotaryEncoder(6,10,11)
+    old = 0    
+    
     while True:
-        time.sleep(1000)   
+        val = int(rotary.getValue()/2.)
+        if old != val:
+            old = val
+            lcd.set_cursor(row=0)
+            lcd.message("%d    "%val)
+        
+        time.sleep(0.1)   
  
 if __name__ == "__main__":
     main()     
